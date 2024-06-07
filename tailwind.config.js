0cb4310df33854cf9e4e5201503a3b9fead8131c
Alessandro Russo <alessandro.russo@datlas.it>
2024-06-07 13:07:22 +0200

    - Removed line-clamp because the version 3.3 of Tailwind is already included

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,7 +68,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     // https://github.com/SamGoody/tailwind-children
     require('tailwind-children'),
   ],
